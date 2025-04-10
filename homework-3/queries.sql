2776bfb924a154ca494a4a5dc4385b5ab7102e77
StasyFree <138008286+StasyFree@users.noreply.github.com>
2025-04-10 09:40:16 +0300

    Update queries.sql

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,11 +2,11 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
-SELECT customers.company_name, CONCAT(employees.first_name, ' ', employees.last_name) AS employee_name
+SELECT customers.company_name, employees.first_name, employees.last_name
 FROM orders
-JOIN customers USING (customer_id)
-JOIN employees USING (employee_id)
-JOIN shippers ON orders.ship_via = shippers.shipper_id
+INNER JOIN employees USING(employee_id)
+INNER JOIN customers USING(customer_id)
+INNER JOIN shippers ON orders.ship_via = shippers.shipper_id
 WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.company_name = 'United Package';
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
@@ -14,17 +14,16 @@ WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.compa
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 
-SELECT product_name, units_in_stock, contact_name, phone
+SELECT products.product_name, products.units_in_stock, suppliers.contact_name, suppliers.phone
 FROM products
-JOIN suppliers USING (supplier_id)
-JOIN categories USING (category_id)
-WHERE discontinued = 0 AND units_in_stock < 25
-  AND category_name IN ('Dairy Products', 'Condiments')
-ORDER BY units_in_stock;
+INNER JOIN suppliers USING(supplier_id)
+INNER JOIN categories USING(category_id)
+WHERE products.discontinued = '0' AND products.units_in_stock < 25 AND categories.category_name IN ('Dairy Products', 'Condiments')
+ORDER BY products.units_in_stock;
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
-SELECT company_name
+SELECT customers.company_name
 FROM customers
 LEFT JOIN orders USING(customer_id)
 WHERE order_id IS NULL;
