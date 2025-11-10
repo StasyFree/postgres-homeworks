d9ce5c60469a6df8b950963a050ab790e860910d
StasyFree <138008286+StasyFree@users.noreply.github.com>
2025-11-10 20:14:13 +0300

    Update alter_northwind.sql

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -7,20 +7,11 @@ ALTER TABLE products ADD CONSTRAINT chk_producrs_discontinued CHECK (discontinue
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
 
-CREATE TABLE discontinued_products AS
-SELECT *
-FROM products
-WHERE discontinued = 1;
+SELECT * INTO discontinued_products FROM products WHERE discontinued=1;
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
 -- Удалим записи в "order_details", которые больше не могут ссылаться на удаляемые продукты, а затем удалим снятые с продажи продукты.
-DELETE FROM order_details
-WHERE product_id IN (
-    SELECT product_id
-    FROM products
-    WHERE discontinued = 1
-);
+DELETE FROM order_details WHERE product_id IN (SELECT product_id FROM discontinued_products);
 
-DELETE FROM products
-WHERE discontinued = 1;
+DELETE FROM products WHERE discontinued = 1;
